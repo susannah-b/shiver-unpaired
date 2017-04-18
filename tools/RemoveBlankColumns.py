3b0716ca5920e415d03f89018ca4f93df4d5c5a6
ChrisHIV <c.wymant@imperial.ac.uk>
2017-04-18 11:07:00 +0100

    Added the --uninformative option.

--- tools/RemoveBlankColumns.py
+++ tools/RemoveBlankColumns.py
@@ -6,7 +6,8 @@ from __future__ import print_function
 ##
 ## Overview:
 ExplanatoryMessage = '''This script removes those columns/positions in alignment
-that consist solely of the gap character "-". Output is printed to stdout.'''
+that are wholly 'blank', i.e. consist solely of the gap character "-". Output is
+printed to stdout.'''
 
 import argparse
 import os
@@ -23,9 +24,11 @@ def File(MyFile):
 ExplanatoryMessage = ExplanatoryMessage.replace('\n', ' ').replace('  ', ' ')
 parser = argparse.ArgumentParser(description=ExplanatoryMessage)
 parser.add_argument('FastaFile', type=File)
-parser.add_argument('-?', '--q-mark', action='store_true', help='''Remove
-columns that only contain the '-' character and/or the '?' character (assumed
-throughout Chris Wymant's code to mean missing coverage).''')
+parser.add_argument('-?', '--q-mark', action='store_true', help='''Also consider
+the '?' character (assumed throughout Chris Wymant's code to mean missing
+coverage) to be blank.''')
+parser.add_argument('-U', '--uninformative', action='store_true', help='''Also
+remove those columns in which all non-blank bases are all identical.''')
 args = parser.parse_args()
 
 # Are we checking just for "-", or also for "?"
@@ -36,12 +39,20 @@ if args.q_mark:
 alignment = AlignIO.read(args.FastaFile, "fasta")
 AlignmentLength = alignment.get_alignment_length()
 for column in reversed(xrange(AlignmentLength)):
-  PureGap = True
+  RemoveThisCol = True
+  FirstBaseSeen = None
   for base in alignment[:, column]:
     if not base in BlankChars:
-      PureGap = False
-      break
-  if PureGap:
+      if args.uninformative:
+        if FirstBaseSeen == None:
+          FirstBaseSeen = base
+        elif base != FirstBaseSeen:
+          RemoveThisCol = False
+          break
+      else:
+        RemoveThisCol = False
+        break
+  if RemoveThisCol:
     alignment = alignment[:, :column] + alignment[:, column+1:]
 
 AlignIO.write(alignment, sys.stdout, 'fasta')

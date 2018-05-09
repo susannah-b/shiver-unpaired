fad14aafab6e586be851e2e043cf9487c1f2a44d
ChrisHIV <chris.wymant@bdi.ox.ac.uk>
2018-05-09 14:16:53 +0100

    Moved private TranslateSeqCoordsToAlnCoords function to ShiverFuncs

--- tools/ShiverFuncs.py
+++ tools/ShiverFuncs.py
@@ -17,3 +17,24 @@ def CalculateReadIdentity(PysamRead, ReferenceSeq):
         NumDeletions += DeletionSize
       LastRefPos = pos
   return float(NumAgreeingBases) / (len(positions) + NumDeletions)
+
+# Stolen from phyloscanner
+def TranslateSeqCoordsToAlnCoords(seq, coords):
+  '''Takes a sequence that contains gaps (in general), and a set of coordinates
+  specified with a respect to that sequence without gaps. The coordinates are
+  translated to their positions in the gappy version of the sequence.
+  e.g. called with the arguments "-a--cg-t-" and [1,2,3], we return [2,5,6].
+  '''
+  TranslatedCoords = [-1 for coord in coords]
+  PositionInSeq = 0
+  for GappyPostitionMin1,base in enumerate(seq):
+    if base != '-':
+      PositionInSeq += 1
+      for i,coord in enumerate(coords):
+        if coord == PositionInSeq:
+          TranslatedCoords[i] = GappyPostitionMin1+1
+      if not -1 in TranslatedCoords:
+        break
+  assert not -1 in TranslatedCoords
+  assert len(TranslatedCoords) == len(coords)
+  return TranslatedCoords

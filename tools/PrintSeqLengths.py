1fc817437046af2c6b0ce0e77877659a24a27fcd
ChrisHIV <chris.wymant@bdi.ox.ac.uk>
2018-05-15 18:01:37 +0100

    Made multiple input fasta files possible

--- tools/PrintSeqLengths.py
+++ tools/PrintSeqLengths.py
@@ -5,8 +5,8 @@ from __future__ import print_function
 ## Acknowledgement: I wrote this while funded by ERC Advanced Grant PBDR-339251
 ##
 ## Overview:
-ExplanatoryMessage = '''This script takes one fasta file as an argument and
-prints the length of each sequence found therein, optionally ignoring gaps.'''
+ExplanatoryMessage = '''This script analyses one or more fasta files,
+printing the length of each sequence found therein.'''
 
 import argparse
 import os
@@ -24,15 +24,13 @@ def File(MyFile):
 # Set up the arguments for this script
 ExplanatoryMessage = ExplanatoryMessage.replace('\n', ' ').replace('  ', ' ')
 parser = argparse.ArgumentParser(description=ExplanatoryMessage)
-parser.add_argument('FastaFile', type=File)
+parser.add_argument('FastaFile', type=File, nargs="+")
 parser.add_argument('-g', '--include-gaps', action='store_true', \
 help='include gap characters, "-" and "?", ignored by default')
 parser.add_argument('-C', '--ignore-lower-case', action='store_true', \
 help="Doesn't count lower case letters.")
 parser.add_argument('-1', '--first-seq-only', action='store_true', \
-help='''Print the length of the first sequence only. Faster for alignments
-containing many sequences (in which case you'll likely want the -g option
-too!).''')
+help='''Look at only the first sequence in each input fasta file.''')
 parser.add_argument('-F', '--fragments', action='store_true', help='''Print the
 length of the 'fragments' of the sequence, i.e. those parts that are separated
 by one or more '?' characters (denoting missing sequence data). If a sequence
@@ -79,49 +77,50 @@ undetermined_start_regex = "^[nN\?]+"
 undetermined_end_regex = "[nN\?]+$"
 
 SeqLengths = []
-for seq in SeqIO.parse(open(args.FastaFile),'fasta'):
+for file_ in args.FastaFile:
+  for seq in SeqIO.parse(open(file_),'fasta'):
 
-  # Check for undetermined starts and ends if desired.
-  seq_as_str = str(seq.seq)
-  if check_undetermined:
-    if args.undetermined_start:
-      datum = [seq.id, get_max_match_length(seq_as_str,
-      undetermined_start_regex)]
-      if args.undetermined_end:
-        datum.append(get_max_match_length(seq_as_str, undetermined_end_regex))
+    # Check for undetermined starts and ends if desired.
+    seq_as_str = str(seq.seq)
+    if check_undetermined:
+      if args.undetermined_start:
+        datum = [seq.id, get_max_match_length(seq_as_str,
+        undetermined_start_regex)]
+        if args.undetermined_end:
+          datum.append(get_max_match_length(seq_as_str, undetermined_end_regex))
+      else:
+        datum = [seq.id, get_max_match_length(seq_as_str, undetermined_end_regex)]
+      SeqLengths.append(datum)
+      if args.first_seq_only:
+        break
+      continue
+
+    # Check the longest gap if desired.
+    if args.longest_gap:
+      longest_gap_length = get_max_match_length(seq_as_str, "[-]+")
+      SeqLengths.append([seq.id, longest_gap_length])
+      if args.first_seq_only:
+        break
+      continue
+
+    if not args.include_gaps:
+      seq.seq = seq.seq.ungap("-")
+      if not args.fragments:
+        seq.seq = seq.seq.ungap("?")
+    if args.ignore_n:
+        seq.seq = seq.seq.ungap("n")
+        seq.seq = seq.seq.ungap("N")
+    if args.ignore_lower_case:
+      seq.seq = ''.join(x for x in seq.seq if not x.islower())
+    if args.fragments:
+      FragLengths = [len(frag) for frag in seq.seq.split('?') if len(frag) > 0]
+      FragLengths = sorted(FragLengths, reverse=True)
+      FragLengths.append(0)
+      SeqLengths.append([seq.id] + FragLengths)
     else:
-      datum = [seq.id, get_max_match_length(seq_as_str, undetermined_end_regex)]
-    SeqLengths.append(datum)
+      SeqLengths.append([seq.id, len(seq.seq)])
     if args.first_seq_only:
       break
-    continue
-
-  # Check the longest gap if desired.
-  if args.longest_gap:
-    longest_gap_length = get_max_match_length(seq_as_str, "[-]+")
-    SeqLengths.append([seq.id, longest_gap_length])
-    if args.first_seq_only:
-      break
-    continue
-
-  if not args.include_gaps:
-    seq.seq = seq.seq.ungap("-")
-    if not args.fragments:
-      seq.seq = seq.seq.ungap("?")
-  if args.ignore_n:
-      seq.seq = seq.seq.ungap("n")
-      seq.seq = seq.seq.ungap("N")
-  if args.ignore_lower_case:
-    seq.seq = ''.join(x for x in seq.seq if not x.islower())
-  if args.fragments:
-    FragLengths = [len(frag) for frag in seq.seq.split('?') if len(frag) > 0]
-    FragLengths = sorted(FragLengths, reverse=True)
-    FragLengths.append(0)
-    SeqLengths.append([seq.id] + FragLengths)
-  else:
-    SeqLengths.append([seq.id, len(seq.seq)])
-  if args.first_seq_only:
-    break
 
 for data in SeqLengths:
   print(' '.join(map(str,data)))

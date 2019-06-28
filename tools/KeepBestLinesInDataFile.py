9d5145e3e4599610178457af0bdde51eb5892bca
ChrisHIV <chris.wymant@bdi.ox.ac.uk>
2019-06-28 16:24:22 +0100

    Minor bug fix: now when cleaning the reads, if none of the forward or none of the reverse reads blast at all, instead of crashing we proceed to mapping (which will likely map none of the read pairs).

--- tools/KeepBestLinesInDataFile.py
+++ tools/KeepBestLinesInDataFile.py
@@ -102,11 +102,6 @@ with open(args.in_file, 'r') as f:
     else:
       rows_to_keep[id_] = (line, sort_value)
 
-# Exit if empty.
-if len(rows_to_keep) == 0:
-  print("Found no data in", args.in_file + ". Quitting.", file=sys.stderr)
-  exit(1)
-
 with open(args.out_file, "w") as f:
   if args.header:
     f.write(header)

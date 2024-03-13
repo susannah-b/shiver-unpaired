c4798db7a62317c28a716d97ea66c869b8fcc894
S Blundell <s.blundell@Susannahs-MacBook-Air.local>
2024-03-13 12:57:35 +0000

    mprove handling of arguments and errors. Separated gene extraction python code to a separate script. Added a check for failed alignments.

--- tools/CodonCorrectionInit.sh
+++ tools/CodonCorrectionInit.sh
@@ -11,32 +11,56 @@ set -o pipefail
 
 UsageInstructions="Arguments for this script:
 (1) A fasta file of annotatable references
+(2) The output folder for the BLAST database and init steps
 "
 
-NumArgsExpected=1
+NumArgsExpected=2
 if [ "$#" -ne "$NumArgsExpected" ]; then
   echo "$UsageInstructions"
   echo "$#" 'arguments specified;' "$NumArgsExpected" 'expected. Quitting.' >&2
   exit 1
 fi
 ReferenceFasta="$1"
-
-# Specific filepath but I think functional as shiver is assumed to be in ~/shiver?
-ReferenceFolder="$HOME/shiver/CodonCorrectionReferences/"
-BLASTnDB="$HOME/shiver/CodonCorrectionReferences/BLASTnDB"
+OutputDirInit="$2" # Not fully implemented this yet - should be read by CC.sh
 
 # Check necessary files exist TODO
+# cd to $OutputDir
+if [[ -d "$OutputDirInit" ]]; then
+  echo "$OutputDirInit exists already; quitting to prevent overwriting." >&2
+  exit 1
+fi
+mkdir -p "$OutputDirInit" && cd "$OutputDirInit" ||
+{ echo "Could not mkdir then cd to $OutputDirInit. Quitting." >&2 ; exit 1 ; }
 
-### Create folders to store the database
-# For now this file is created, if including a set of references with shiver you would presumably already create this folder
-# Need to check other files required to be in this folder - it might be unecessary if there are now only 1-2 files
-if [ ! -d "$ReferenceFolder" ]; then
-	mkdir "$ReferenceFolder"
-fi || { echo "Unable to create reference folder. Quitting."  >&2 ; exit 1 ; }
+if [[ -f "$ReferenceFasta" ]]; then
+# Check reference file is a .fasta
+  if [[ "$ReferenceFasta" != *.fasta ]]; then 
+    echo "Reference file $ReferenceFasta is not a fasta file. Quitting." >&2
+    exit 1
+  fi
 
+  ### Check the reference file contains sequences
+  # Count sequence number in reference and sample
+  Ref_SeqNumber=$(grep '^>' "$ReferenceFasta" | wc -l | awk '{$1=$1};1')
+  if [[ "$Ref_SeqNumber" == 0 ]]; then
+    echo "Reference file $ReferenceFasta contains no sequences. Quitting." >&2
+    exit 1
+  fi
+else
+  echo "Reference file $ReferenceFasta does not exist. Check specified filepath. Quitting." >&2
+  exit 1
+fi
+
+BLASTnDB=""$OutputDirInit"/BLASTnDB"
 if [ ! -d "$BLASTnDB" ]; then
   mkdir "$BLASTnDB"
 fi || { echo "Unable to create database folder. Quitting."  >&2 ; exit 1 ; }
+
+# Copy reference file to working directory
+# figure out non-specific shiver path
+Ref_Copied=""$OutputDirInit"/CC_References.fasta"
+cp "$ReferenceFasta" "$Ref_Copied" || { echo "Failed to copy $ReferenceFasta to the output"\
+  "directory. Quitting." >&2; exit 1; }
 
 ### Create a whole genome database to BLASTn to
 # should really call the reference file as an arg but set that up later
